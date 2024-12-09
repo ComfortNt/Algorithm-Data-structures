e7192a6ba89a36977969d48b619b644f3c91c45c
ComfortNt <128999583+ComfortNt@users.noreply.github.com>
2024-12-09 08:36:10 +0200

    fin
    touch-ups

--- Algorithm.java
+++ Algorithm.java
@@ -5,7 +5,7 @@ import  java.util.*;
 // popular methods include push and pop.
 // push() adds an item to the top of the stack.
 // pop() removes an item from the top of the stack.
-// other methods -> stack.empty() , stack.peek()
+// other methods -> stack.empty() returns a boolean , stack.peek() returns item without removing it from the stack
 
 
 class Algorithm{
@@ -17,11 +17,11 @@ class Algorithm{
         mystack.push("mystack-item-1");
         mystack.push("mystack-item-2");
 
-        
-        mystack.peek();
+        String favItem = mystack.peek();
+        mystack.pop();  
         // removes index 2/(mystack-item-2) cause 2 is at the top of the stack.
         for( String i : mystack){
-            System.out.println(i);
+            System.out.println(i + favItem);
         };
 
 
