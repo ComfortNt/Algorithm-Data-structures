ec91891c6afc07c67d0930462b9887dfeee16ec7
ComfortNt <ntcomfort94@gmail.com>
2024-12-29 08:59:56 +0200

    FizzBuzz
    ...

--- Alogrithm.js
+++ Alogrithm.js
@@ -72,7 +72,29 @@ console.log(BinarySearch(Sorted , 11));
 
    console.log( Factorial(9));
 
-   // Selection sort
+   // FizzBuzz
+
+   const FizzBuzz=(num)=>{
+
+    for(let i = 1 ; i <= num ; i++){
+
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+            
+        }else if( i % 3 === 0){
+            console.log("Fizz");
+            
+        }else if(i % 5 === 0){
+            console.log("Buzz");
+            
+        }else{
+            console.log(i);
+            
+        }
+    }
+   }
+
+   FizzBuzz(22);
 
 
    
